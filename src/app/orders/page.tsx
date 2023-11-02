7515322ce838c2a6ecdfe80844eb76073f58df29
Anton Petrovic <78913554+petrovicanton@users.noreply.github.com>
2023-11-02 21:55:19 +0100

    Update page.tsx

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,5 +1,4 @@
 import { getServerSession } from "next-auth/next";
-
 import HeroSection from "@/components/HeroSection/HeroSection";
 import { fetchOrder } from "@/libs/apis";
 import { authOptions } from "@/libs/auth";
@@ -20,66 +19,68 @@ const Orders = async () => {
   return (
     <div>
       <HeroSection />
-      <div className="relative overflow-x-auto px-6 sm:px-12 md:px-20 lg:px-36 pb-40 mt-24">
+      <div className="relative overflow-x-auto px-4 sm:px-8 md:px-12 lg:px-16 xl:px-24 2xl:px-32 pb-20 mt-24">
         <h1
           id="currentOrder"
-          className="text-center text-4xl font-bold text-gray-300 mb-10"
+          className="text-center text-4xl font-bold text-gray-300 mb-6 sm:mb-8 md:mb-10 lg:mb-12"
         >
           Current Order:
         </h1>
-        <table className="w-full text sm text-left text-gray-400">
-          <thead className="text-xs uppercase bg-gray-700 text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product(s) name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Unit Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Order Status
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Total Price
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {orderData.map((order: any) => {
-              const totalPrice = order.items.reduce((acc: any, item: any) => {
-                const itemPrice = item.quantity * item.movie.price;
-                return acc + itemPrice;
-              }, 0);
+        <div className="overflow-x-auto">
+          <table className="w-full text-sm text-left text-gray-400">
+            <thead className="text-xs uppercase bg-gray-700 text-gray-400">
+              <tr>
+                <th scope="col" className="px-4 sm:px-6 py-2 sm:py-3">
+                  Product(s) name
+                </th>
+                <th scope="col" className="px-4 sm:px-6 py-2 sm:py-3">
+                  Unit Price
+                </th>
+                <th scope="col" className="px-4 sm:px-6 py-2 sm:py-3">
+                  Order Status
+                </th>
+                <th scope="col" className="px-4 sm:px-6 py-2 sm:py-3">
+                  Total Price
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {orderData.map((order: any) => {
+                const totalPrice = order.items.reduce((acc: any, item: any) => {
+                  const itemPrice = item.quantity * item.movie.price;
+                  return acc + itemPrice;
+                }, 0);
 
-              return (
-                <tr
-                  key={order._id}
-                  className="border-b bg-gray-800 border-gray-700"
-                >
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium whitespace-nowrap text-white"
+                return (
+                  <tr
+                    key={order._id}
+                    className="border-b bg-gray-800 border-gray-700"
                   >
-                    {order.items.map((item: any) => (
-                      <span key={item._id}>
-                        {item.movie.name} ({item.quantity}) <br />
-                      </span>
-                    ))}
-                  </th>
-                  <td className="px-6 py-4">
-                    {order.items.map((item: any) => (
-                      <span key={item._id}>
-                        $ {item.movie.price} <br />
-                      </span>
-                    ))}
-                  </td>
-                  <td className="px-6 py-4">{order.orderStatus}</td>
-                  <td className="px-6 py-4">$ {totalPrice}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                    <td
+                      scope="row"
+                      className="px-4 sm:px-6 py-4 font-medium whitespace-nowrap text-white"
+                    >
+                      {order.items.map((item: any) => (
+                        <span key={item._id}>
+                          {item.movie.name} ({item.quantity}) <br />
+                        </span>
+                      ))}
+                    </td>
+                    <td className="px-4 sm:px-6 py-4">
+                      {order.items.map((item: any) => (
+                        <span key={item._id}>
+                          $ {item.movie.price} <br />
+                        </span>
+                      ))}
+                    </td>
+                    <td className="px-4 sm:px-6 py-4">{order.orderStatus}</td>
+                    <td className="px-4 sm:px-6 py-4">$ {totalPrice}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
